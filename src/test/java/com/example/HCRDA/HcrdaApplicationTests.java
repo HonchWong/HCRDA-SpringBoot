b4fd66930562b323574ad721751f8b5b285f7dae
honchwong <honchwong404@gmail.com>
2018-10-05 23:49:14 +0800

    add project

---
+++ src/test/java/com/example/HCRDA/HcrdaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.HCRDA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HcrdaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
